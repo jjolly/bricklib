fe7dde28b3d40eb5da9e6b66c662049a920cd711
John Jolly <john.jolly@utah.edu>
2022-05-25 14:01:08 -0600

    error handling in build.sh

--- examples/external/build.sh
+++ examples/external/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-mkdir build
+if ! which cmake > /dev/null; then
+  if which spack > /dev/null; then
+    spack load cmake || exit 1
+  else
+    echo cmake not found
+    exit 1
+  fi
+fi
+
+mkdir build || exit 1
 cd build
-cmake ..
-cmake --build .
-./example
+cmake .. || exit 1
+cmake --build . || exit 1
+./example || exit 1
